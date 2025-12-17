7935f9d78fae48727e07769426e2ff03272329d3
yerin-lab <chayerin_@naver.com>
2025-12-18 02:09:46 +0900

    [#6] build : emojis svg import

--- src/components/EmojiSlide.tsx
+++ src/components/EmojiSlide.tsx
@@ -1,7 +1,28 @@
 import { motion } from 'framer-motion';
+import Happy from '../assets/emojis/center/Happy.svg?react';
+import Afraid from '../assets/emojis/center/Afraid.svg?react';
+import Angry from '../assets/emojis/center/Angry.svg?react';
+import Boredom from '../assets/emojis/center/Boredom.svg?react';
+import Joyful from '../assets/emojis/center/Joyful.svg?react';
+import Sad from '../assets/emojis/center/Sad.svg?react';
+import Shameful from '../assets/emojis/center/Shameful.svg?react';
+import Smile from '../assets/emojis/center/Smile.svg?react';
+import Unrest from '../assets/emojis/center/Unrest.svg?react';
+import Worried from '../assets/emojis/center/Worried.svg?react';
+
+export const emojis = [];
 
 const EmojiSlide = () => {
-  return <h1>Emoji Slide</h1>;
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  return (
+    <motion.div initial='hidden' animate='visible' variants={variants}>
+      <Happy />
+    </motion.div>
+  );
 };
 
 export default EmojiSlide;
