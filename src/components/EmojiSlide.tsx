ba39b333cb9ed04b97eda38ed5865f7edfd8b9b9
yerin-lab <chayerin_@naver.com>
2025-12-18 01:15:32 +0900

    [#6] build : emojis 폴더 생성 및 emoji svg 추가

---
+++ src/components/EmojiSlide.tsx
@@ -0,0 +1,7 @@
+import { motion } from 'framer-motion';
+
+const EmojiSlide = () => {
+  return <h1>Emoji Slide</h1>;
+};
+
+export default EmojiSlide;
--- src/pages/ExamplePage.tsx
+++ src/pages/ExamplePage.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/common/Button';
 import Dropdown from '@/components/common/Dropdown';
+import EmojiSlide from '@/components/EmojiSlide';
 
 const yearItems = ['2021', '2022', '2023', '2024', '2025'];
 const monthItems = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
@@ -14,6 +15,9 @@ const ExamplePage = () => {
       <div className='flex items-center gap-3'>
         <Dropdown items={yearItems} className='w-105' />
         <Dropdown items={monthItems} />
+      </div>
+      <div>
+        <EmojiSlide />
       </div>
     </div>
   );
