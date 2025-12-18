8c5817a915c61170197808b252b420a4df21a37a
yerin-lab <chayerin_@naver.com>
2025-12-18 21:57:45 +0900

    [#6] refactor : index.tsx에 이모지 분리

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,1 +1,14 @@
 // init
+
+import Happy from '@/assets/emojis/Happy.svg?react';
+import Afraid from '@/assets/emojis/Afraid.svg?react';
+import Angry from '@/assets/emojis/Angry.svg?react';
+import Boredom from '@/assets/emojis/Boredom.svg?react';
+import Joyful from '@/assets/emojis/Joyful.svg?react';
+import Sad from '@/assets/emojis/Sad.svg?react';
+import Shameful from '@/assets/emojis/Shameful.svg?react';
+import Smile from '@/assets/emojis/Smile.svg?react';
+import Unrest from '@/assets/emojis/Unrest.svg?react';
+import Worried from '@/assets/emojis/Worried.svg?react';
+
+export { Happy, Afraid, Angry, Boredom, Joyful, Sad, Shameful, Smile, Unrest, Worried };
--- src/components/EmojiSlide.tsx
+++ src/components/EmojiSlide.tsx
@@ -1,14 +1,16 @@
 import { LayoutGroup, motion } from 'framer-motion';
-import Happy from '../assets/emojis/Happy.svg?react';
-import Afraid from '../assets/emojis/Afraid.svg?react';
-import Angry from '../assets/emojis/Angry.svg?react';
-import Boredom from '../assets/emojis/Boredom.svg?react';
-import Joyful from '../assets/emojis/Joyful.svg?react';
-import Sad from '../assets/emojis/Sad.svg?react';
-import Shameful from '../assets/emojis/Shameful.svg?react';
-import Smile from '../assets/emojis/Smile.svg?react';
-import Unrest from '../assets/emojis/Unrest.svg?react';
-import Worried from '../assets/emojis/Worried.svg?react';
+import {
+  Angry,
+  Afraid,
+  Boredom,
+  Happy,
+  Joyful,
+  Sad,
+  Shameful,
+  Smile,
+  Unrest,
+  Worried,
+} from '@/assets';
 import { useState } from 'react';
 import type React from 'react';
 
