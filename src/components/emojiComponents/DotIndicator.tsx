a66dc4a57ce5199b72539c1e007e40d1e90869ec
yerin-lab <chayerin_@naver.com>
2025-12-18 18:40:46 +0900

    [#21] build : clsx 임포트

--- src/components/emojiComponents/DotIndicator.tsx
+++ src/components/emojiComponents/DotIndicator.tsx
@@ -1,3 +1,17 @@
-const DotIndicator = () => {};
+import clsx from 'clsx';
+
+const DotIndicator = ({ total = 5, filled }: { total?: number; filled: number }) => {
+  return (
+    <div>
+      {Array.from(
+        { length: total }.map((_, i) => {
+          const isFilled = i < filled;
+
+          return <span key={i} className={} />;
+        }),
+      )}
+    </div>
+  );
+};
 
 export default DotIndicator;
