73dfbccec527a7c02af61289544529001b24fef7
wldsmdl7 <wldmsdl5835@gmail.com>
2025-12-18 18:45:03 +0900

    [#21] style : 컴포넌트 모든 너비 값 피그마와 통일

--- src/constants/analysisData.ts
+++ src/constants/analysisData.ts
@@ -1,5 +1,5 @@
 export const ANALYSIS_DATA = [
-  { id: 'Happy', aiScore: 90, userScore: 80 },
+  { id: 'Happy', aiScore: 90, userScore: 40 },
   { id: 'Smile', aiScore: 75, userScore: 65 },
   { id: 'Unrest', aiScore: 60, userScore: 50 },
   { id: 'Afraid', aiScore: 45, userScore: 30 },
