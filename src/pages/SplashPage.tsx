33c74d6843b60f8d7b692cb5ea80ebddb9cfa05c
yerin-lab <chayerin_@naver.com>
2025-12-19 21:44:43 +0900

    [#55] style : 스플래쉬 애니메이션 추가

--- src/pages/SplashPage.tsx
+++ src/pages/SplashPage.tsx
@@ -1,7 +1,9 @@
 import Dropdown from '@/components/common/Dropdown';
 import Button from '@/components/common/Button';
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 import { Splash1Icon, Splash2Icon } from '@/assets';
+import { fa } from 'zod/v4/locales';
 
 const YearItems = ['Text', '2021', '2022', '2023', '2024', '2025', ''];
 const MonthItems = [
@@ -22,6 +24,11 @@ const MonthItems = [
   '',
 ];
 
+const fade = {
+  hidden: { opacity: 0, y: 0 },
+  show: { opacity: 1, y: 0 },
+};
+
 const SplashPage = () => {
   const [year, setYear] = useState('Text');
   const [month, setMonth] = useState('Text');
@@ -31,11 +38,24 @@ const SplashPage = () => {
 
   return (
     <div className='relative min-h-screen w-full overflow-hidden bg-[#0F0F10]'>
-      <Splash1Icon className='absolute left-0 top-1/2 -translate-y-1/2 w-125 md:w-200 pointer-events-none' />
-      <Splash2Icon className='absolute right-0 top-[47%] -translate-y-1/2 translate-x-25 w-105 md:w-130 pointer-events-none' />
+      <motion.div
+        initial='hidden'
+        animate='show'
+        variants={fade}
+        transition={{ duration: 3, ease: 'easeOut', delay: 0.1 }}
+        className='pointer-events-none'
+      >
+        <Splash1Icon className='absolute left-0 top-1/2 -translate-y-1/2 w-190' />
+      </motion.div>
       <div className='relative z-10 flex min-h-screen flex-col justify-between px-8 py-10'>
         <div className='flex flex-1 items-center justify-center'>
-          <div className='flex items-center gap-4 pl-90 pt-10'>
+          <motion.div
+            initial='hidden'
+            animate='show'
+            variants={fade}
+            transition={{ duration: 3, ease: 'easeOut', delay: 0.5 }}
+            className='flex items-center gap-4 pl-90 pt-10'
+          >
             <p className='text-white text-3xl font-pretendard font-semibold whitespace-nowrap'>
               Making sense of your
             </p>
@@ -49,10 +69,18 @@ const SplashPage = () => {
               className='min-w-35'
               onSelect={(value: string) => setMonth(value)}
             />
-          </div>
+          </motion.div>
+          <motion.div
+            initial='hidden'
+            animate='show'
+            variants={fade}
+            transition={{ duration: 3, ease: 'easeOut', delay: 1 }}
+          >
+            <Splash2Icon className='absolute right-0 top-[47%] -translate-y-1/2 translate-x-24 w-120 pointer-events-none' />
+          </motion.div>
         </div>
         <div className='flex items-center justify-between'>
-          <Button type='button' className='w-62.5 bg-[#E1E0E0] text-[#7C7979]'>
+          <Button type='button' className='w-62.5 !bg-[#E1E0E0] text-[#7C7979]'>
             Back
           </Button>
           <Button
