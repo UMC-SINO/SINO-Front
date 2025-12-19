500e022f4c23c000a7e3acbdb5b1f844523818b9
wldsmdl7 <wldmsdl5835@gmail.com>
2025-12-19 10:58:34 +0900

    [#18] refactor : 필터링 버튼 플로우  변경

--- src/pages/CardListPage.tsx
+++ src/pages/CardListPage.tsx
@@ -2,11 +2,9 @@ import { NSCardList } from '@/components/common/NSList/NSCardList';
 import { NOISE_CARDS, SIGNAL_CARDS } from '@/data/nscard';
 
 const CardListPage = () => {
-  const signalBadges = [{ label: 'Year' }, { label: 'Month' }, { label: 'Bookmark' }];
-
   return (
     <div className='text-4xl min-h-dvh flex items-center justify-center font-semibold flex-row gap-10'>
-      <NSCardList cards={SIGNAL_CARDS} title='Signal' badges={signalBadges} />
+      <NSCardList cards={SIGNAL_CARDS} title='Signal' />
       <NSCardList cards={NOISE_CARDS} title='Noise' />
     </div>
   );
