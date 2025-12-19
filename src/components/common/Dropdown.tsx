37e9dc416d1e4fe4f53826303dd758b866a2e4ad
jeniferkim <jeniferkim1673@naver.com>
2025-12-19 12:10:00 +0900

    [#38] chore: framer-motion에서 useScroll 임포트

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence, useScroll } from 'framer-motion';
 import clsx from 'clsx';
 import { ChevronDown } from 'lucide-react';
 
