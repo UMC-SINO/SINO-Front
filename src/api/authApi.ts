64dc2db9f079562825935e188fc157b87a26fb15
jeniferkim <jeniferkim1673@naver.com>
2025-12-20 19:51:20 +0900

    [#63] fix: 함수 구조 변경

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,24 +1,17 @@
-// src/apis/authApi.ts
 import type { ApiResponse, NicknameRequest } from '@/types/auth';
 import { axiosInstance } from './api';
 
-export const postCheckNickname = {
-  checkNickname: async (body: NicknameRequest) => {
-    const { data } = await axiosInstance.post<ApiResponse>('/api/auth/check-nickname', body);
-    return data;
-  },
+export const postCheckNickname = async (body: NicknameRequest) => {
+  const { data } = await axiosInstance.post<ApiResponse>('/api/auth/check-nickname', body);
+  return data;
 };
 
-export const postSignup = {
-  signup: async (body: NicknameRequest) => {
-    const { data } = await axiosInstance.post<ApiResponse>('/api/auth/signup', body);
-    return data;
-  },
+export const postSignup = async (body: NicknameRequest) => {
+  const { data } = await axiosInstance.post<ApiResponse>('/api/auth/signup', body);
+  return data;
 };
 
-export const postLogin = {
-  login: async (body: NicknameRequest) => {
-    const { data } = await axiosInstance.post<ApiResponse>('/api/auth/login', body);
-    return data;
-  },
+export const postLogin = async (body: NicknameRequest) => {
+  const { data } = await axiosInstance.post<ApiResponse>('/api/auth/login', body);
+  return data;
 };
